d98980c724100a8d2c7c0b578a5bb4182b379d3c
DominicBae <baehope823@hanmail.net>
2024-07-03 22:52:47 +0900

    3rd commit

--- main.js
+++ main.js
@@ -17,8 +17,11 @@ addButton.addEventListener("click", addTask)
 taskInput.addEventListener("focus", function () { taskInput.value = "" })
 
 function addTask() {
-    let taskContent = taskInput.value;
-    taskList.push(taskContent);
+    let task = {
+        taskContent: taskInput.value,
+        isComplete: false
+    };
+    taskList.push(task);
     console.log(taskList);
     render();
 }
@@ -27,7 +30,7 @@ function render() {
     let resultHTML = '';
     for (let i = 0; i < taskList.length; i++) {
         resultHTML += `<div class="task">
-                    <div>${taskList[i]}</div>
+                    <div>${taskList[i].taskContent}</div>
                     <div>
                         <button>Check</button>
                         <button>Delete</button>
